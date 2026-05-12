a16846dcd85777fb266fe297aa4151db12f29550
Robert Pelletier <ropelletier@rsu87.org>
2026-05-11 20:59:19 -0400

    Update api: sync.js

--- api/routes/sync.js
+++ api/routes/sync.js
@@ -149,7 +149,9 @@ async function getChangedCardholders(adsLiteral) {
   const rows = await query(`
     SELECT c.PkData AS id, c.UserName AS name, c.State AS state, c.Email AS email,
       c.CreationDate AS createdAt, c.CardNumberCount AS cardCount,
-      c.CardInfo1, c.CardInfo2, c.CardInfo3, c.CardInfo4, c.CardInfo5, c.CardInfo20 AS UUID,
+      c.CardInfo1, c.CardInfo2, c.CardInfo3, c.CardInfo4, c.CardInfo5,
+      c.CardInfo3 AS GMKey, c.CardInfo4 AS CESKey, c.CardInfo8 AS CMSKey, c.CardInfo9 AS SESKey,
+      c.CardInfo20 AS UUID,
       c.FkCardType AS cardTypeId, ct.Description1 AS cardTypeName,
       ic.FkICDataAccessLevel AS accessLevelId, al.Description1 AS accessLevelName,
       c.TransactionTag
